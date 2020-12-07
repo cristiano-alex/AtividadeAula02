3e4dda38414a43f95ed44cc48f0cdcecf7934133
cristiano alex dos santos <cristiano_alex@live.com>
2020-12-07 20:21:30 -0300

    Novas Implementacoes

--- AtividadeAula02/Controllers/HomeController.cs
+++ AtividadeAula02/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace AtividadeAula02.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult CEP()
+        {
+            return View();
+        }
     }
 }
---
+++ AtividadeAula02/Models/CEP.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeAula02.Models
+{
+    public class CEP
+    {
+        public int Id { get; set; }
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Cod_IBGE_Estado { get; set; }
+        public string Cod_IBGE_Cidade { get; set; }
+
+        public DateTime CriadoEm { get; set; }
+    }
+}

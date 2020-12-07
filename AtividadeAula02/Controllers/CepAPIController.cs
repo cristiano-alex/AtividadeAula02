f96e72c704fd71c0df112c70d61305179ce882ea
cristiano alex dos santos <cristiano_alex@live.com>
2020-12-07 20:54:20 -0300

    implementacao API

---
+++ AtividadeAula02/Controllers/CepAPIController.cs
@@ -0,0 +1,31 @@
+﻿using AtividadeAula02.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadeAula02.Controllers
+{
+    [Route("api/cep")]
+    [ApiController]
+    public class CepAPIController : ControllerBase
+    {
+        private List<CEP> lista = new List<CEP>
+        {
+
+            new CEP {Cep = "81720240", Bairro= "Alto boqueirao"},
+            new CEP {Cep = "76776734", Bairro= "teste 2" }
+        };
+
+
+        // GET: api/cep/listar
+        [HttpGet]
+        [Route("listar")]
+        public List<CEP> listar()
+        {
+            return lista;
+        }
+    }
+}
